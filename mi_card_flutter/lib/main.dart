85057605c47fa4d102eb199a365617ced5de0d77
Pratik Bhadane <pratikbhadane24@gmail.com>
2022-01-05 01:58:24 +0530

    finalized app

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -11,33 +11,83 @@ class Myapp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black87,
         body: SafeArea(
-            child: Column(
-          children: [
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/pratik.jpg'),
-            ),
-            Text(
-              'Pratik Bhadane',
-              style: TextStyle(
-                fontFamily: 'ZCOOLQingKeHuangYou',
-                color: Colors.white,
-                fontSize: 40.0,
-                letterSpacing: 2,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/pratik.jpg'),
               ),
-            ),
-            Text(
-              'Software Developer',
-              style: TextStyle(
-                fontFamily: 'Srisakdi',
-                color: Colors.white,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1.2,
+              Text(
+                'Pratik Bhadane',
+                style: TextStyle(
+                  fontFamily: 'ZCOOLQingKeHuangYou',
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  letterSpacing: 1.5,
+                ),
               ),
-            )
-          ],
-        )),
+              Text(
+                'Software Developer',
+                style: TextStyle(
+                  fontFamily: 'Srisakdi',
+                  color: Colors.grey,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.2,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 30.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.black87,
+                  ),
+                  title: Text(
+                    '+91-9096480226',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 17.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 30.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.black87,
+                  ),
+                  title: Text(
+                    'pratikbhadane24@gmail.com',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 17.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
